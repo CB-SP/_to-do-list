4719f71e6d44506095f60870c6d0f2c46a735ca4
sérgioSp <sergiosapalalo16@gmail.com>
2026-03-26 11:21:47 +0100

    Building UI

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -6,12 +6,13 @@ function App() {
 
   return (
     <main className="main">
-      <Header></Header>
-      
-      <section className="cardsArea">
-      <Card title="Criadas" desc="Nenhuma tarefa" category='created'></Card>
-
-      </section>
+        <Header></Header>
+        
+        <section className="cardsArea">
+        <Card title="Criadas" desc="Nenhuma tarefa" category="created" className="card"></Card>
+        <Card title="Pendentes" desc="Nenhuma tarefa" category="pendent"></Card>
+        <Card title="Canceladas" desc="Nenhuma tarefa" category="canceled"></Card>
+        </section>
     </main>
 
   )
